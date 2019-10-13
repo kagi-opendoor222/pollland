c62d93ce4148fe9142cf3af6f5ae94e51ec8e11c
kagi-opendoor222 <38743710+kagi-opendoor222@users.noreply.github.com>
2019-10-13 14:46:38 +0900

    redirect to origin page after LOGIN by omniauth

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -7,11 +7,25 @@ import AgendaBoard from "./Components/AgendaBoard";
 import PostAgendaForm from "./Components/PostAgendaForm";
 
 class App extends React.Component {
+  toStringForUrl(obj){
+    const keys = Object.keys(obj)
+    const queryParamsStr =  keys.reduce((accumulator, key)=>{
+      return accumulator + key + "=" + obj[key].toString() + "&"
+    }, "")
+    return queryParamsStr.slice(0, -1);
+  }
+  loginByOmniAuth(provider){
+    const url = `http://localhost:4000/auth/${provider}`;
+    const params = {origin: window.location}
+    const paramsForUrl = this.toStringForUrl(params)
+    location.href = url + "?"+ paramsForUrl
+  }
   render(){
     return(
       <Router>
         <Header />
         <div className="page-container">
+          <Link onClick={()=>this.loginByOmniAuth("twitter")}>Login by Twitter</Link>
           <Switch>
             <Route exact path="/" component={GlobalContainer} />
             <Route path="/agendas/new" component={PostAgendaForm} />
