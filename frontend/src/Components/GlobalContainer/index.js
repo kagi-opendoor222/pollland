7bc4e0f3c07b43b7b309807fb7e14569cac5d07f
kagi-opendoor222 <38743710+kagi-opendoor222@users.noreply.github.com>
2019-11-16 01:27:17 +0900

    add PrivateLink to PostAgendaForm

--- frontend/src/Components/GlobalContainer/index.js
+++ frontend/src/Components/GlobalContainer/index.js
@@ -3,17 +3,32 @@ import UserDashBoard from "./UserDashBoard";
 import AgendaList from "./AgendaList";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
+const PrivateLink = (props) =>{
+  let linkComponent
+  if(props.user.isLoggedIn()){
+    linkComponent = <Link to={props.to} className={props.className}>
+                      {props.children}
+                    </Link>
+  }else{
+    linkComponent = <Link to={""} className={props.className} onClick={() => props.handleAddFlash({text: "ログインが必要です", type: "error"})}> 
+                      {props.children}
+                    </Link>
+  }
+  return(
+    linkComponent
+  )
+}
+
 const GlobalContainer = (props)=>{
-  console.log("aaa", props.user)
   return(
     <div className="contents-container">
       <UserDashBoard />
       <div className="main-content">
         <div className="main-content-top">
           <h2>投票</h2>
-          <Link to="/agendas/new" className="main-content-top__post-agenda-button">
+          <PrivateLink to="/agendas/new" className="main-content-top__post-agenda-button" user={props.user} handleAddFlash={props.handleAddFlash} >
             <span>テーマを投稿</span>
-          </Link>
+          </PrivateLink>
         </div>
         <div className="main-content-board">
           <AgendaList user={props.user}/>
